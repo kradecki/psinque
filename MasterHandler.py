c3b1355c5f3a00c32ffa5ef73bb9d5c81288a0d6
Stanisław Raczyński <sutashu@gmail.com>
2012-07-09 19:26:38 +0200

    Basic functionality works.

--- MasterHandler.py
+++ MasterHandler.py
@@ -2,14 +2,9 @@
 import os
 import logging
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
-
-from django.conf import settings
-from django.utils import translation
 
 from UserDataModels import UserSettings
 
@@ -24,34 +19,37 @@ class MenuEntry:
 
 class MasterHandler(webapp.RequestHandler):
   '''This is the base class for all handlers.'''
-  '''It prepares all the information for the top bar.'''
+  '''It prepares all data for the top menu bar.'''
     
   def sendTopTemplate(self, prettify = False, activeEntry = ""):
-      
+
+    if not users.get_current_user():  # user not logged in
+      self.redirect("/login")
+      return
+
     menuentries = [
-      MenuEntry("profile", translation.ugettext("Visiting card")),
-      MenuEntry("notifications", translation.ugettext("Notifications")),
-      MenuEntry("groups", translation.ugettext("Groups")),
-      MenuEntry("contacts", translation.ugettext("Contacts")),
-      MenuEntry("settings", translation.ugettext("Settings"))
+      MenuEntry("profile", "Visiting card"),
+      MenuEntry("notifications", "Notifications"),
+      MenuEntry("groups", "Groups"),
+      MenuEntry("contacts", "Contacts"),
+      MenuEntry("settings", "Settings")
     ]
     if activeEntry != "":
-      activeEntry = translation.ugettext(activeEntry)
       for entry in menuentries:
         if entry.title == activeEntry:
-           entry.entryclass = "active"
+           entry.entryclass = "active"  # mark menu item as active
     
     if prettify:
         topTemplatePath = os.path.join(os.path.dirname(__file__), 'templates/topTemplate_Prettify.html')
     else:
         topTemplatePath = os.path.join(os.path.dirname(__file__), 'templates/topTemplate.html')
-    self.response.out.write(translation.ugettext(template.render(topTemplatePath,
+    self.response.out.write(template.render(topTemplatePath,
         dict(self.getUserVariables().items() + {'menuentries': menuentries}.items()
-    ))))
+    )))
 
   def sendBottomTemplate(self):
     bottomTemplatePath = os.path.join(os.path.dirname(__file__), 'templates/bottomTemplate.html')
-    self.response.out.write(translation.ugettext(template.render(bottomTemplatePath, None)))
+    self.response.out.write(template.render(bottomTemplatePath, None))
 
   def getUserVariables(self):
     user = users.get_current_user()
@@ -59,20 +57,15 @@ class MasterHandler(webapp.RequestHandler):
         return {
           'username': user.nickname(),
           'loginurl': users.create_logout_url(self.request.uri),       
-          'loginurl_linktext': translation.ugettext("Logout"),
+          'loginurl_linktext': "Logout",
           'settings': True
         }
     else:
-        return {
-          'username': "",
-          'loginurl': users.create_login_url(self.request.uri),       
-          'loginurl_linktext': translation.ugettext("Login"),
-          'settings': False
-        }
+        raise Exception("User not logged in.")   # this should never happen, because sendTopTemplate() redirects to /login earlier
 
   def getLanguage(self):
     user = users.get_current_user()
-    query = UserProfile.all()
+    query = UserSettings.all()
     userProfile = query.filter("user =", user).get()
     if not userProfile:  # no user profile registered yet
       self.LANGUAGE_CODE = "en"
@@ -80,42 +73,27 @@ class MasterHandler(webapp.RequestHandler):
       self.LANGUAGE_CODE = userProfile.preferredLanguage
       logging.error("Changed language to " + settings.LANGUAGE_CODE)
 
-  def activateTranslation(self, language = ""):
-    translation.activate(language)
-    if language == "":
-      self.getLanguage()
-    else:
-      self.LANGUAGE_CODE = language
-    translation.activate(self.LANGUAGE_CODE)
-
-  def deactivateTranslation(self):
-    translation.deactivate()
-
   def sendContent(self, templateName, templateVariables = None):
     path = os.path.join(os.path.dirname(__file__), templateName)
-    self.response.out.write(translation.ugettext(template.render(path, templateVariables)))
+    self.response.out.write(template.render(path, templateVariables))
 
   def render(self, activeEntry, templateName, templateVariables = None):
-    MasterHandler.activateTranslation(self)
-    MasterHandler.sendTopTemplate(self, activeEntry = activeEntry)
-    MasterHandler.sendContent(self, templateName, templateVariables)
-    MasterHandler.sendBottomTemplate(self)   
-    MasterHandler.deactivateTranslation(self)
+    self.sendTopTemplate(activeEntry = activeEntry)
+    self.sendContent(templateName, templateVariables)
+    self.sendBottomTemplate()
 
 class StaticMasterHandler(MasterHandler):
   '''A simple class for handling static pages'''
   templateName = ""
   activeEntry  = ""
-    
+  
   def __init__(self, templateName, activeEntry = ""):
     self.templateName = templateName
     self.activeEntry  = activeEntry
     super(StaticMasterHandler, self).__init__()
     
   def get(self):
-    MasterHandler.activateTranslation(self)
     self.sendTopTemplate(activeEntry = self.activeEntry)
     path = os.path.join(os.path.dirname(__file__), self.templateName)
     self.response.out.write(template.render(path, None))
     self.sendBottomTemplate()
-    MasterHandler.deactivateTranslation(self)
--- StartPage.py
+++ StartPage.py
@@ -7,17 +7,27 @@ from google.appengine.api import users
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
 
-from UserDataModels import UserProfile
+from MasterHandler import MasterHandler
 
-class StartPage(DictionaryHandler):
-
+class StartPage(webapp.RequestHandler):
   def get(self):
-
     user = users.get_current_user()
     if not user:    # user not logged in
-        self.redirect('/login')
+        self.redirect("/login")
     else:
         self.redirect('/profile')
+
+class Login(webapp.RequestHandler):
+  def get(self):
+    self.response.out.write(template.render(os.path.join(os.path.dirname(__file__), "templates/login.html"),
+                                            {'loginurl': users.create_login_url("/profile")}
+                                           ))
+
+class ViewProfile(MasterHandler):
+  def get(self):
+    MasterHandler.sendTopTemplate(self, activeEntry = "Visiting card")
+    self.response.out.write('<div id="content">Under construction...</div')
+    MasterHandler.sendBottomTemplate(self)
 
 application = webapp.WSGIApplication([
   ('/', StartPage),
