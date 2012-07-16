44991cb23f51ae5e21bed68e2c58c77918148140
Krzysztof Radecki <krzysztof.radecki@gmail.com>
2012-07-16 21:51:19 +0200

    Prototype of the Settings; Modified Routes in the app.yaml; Commented out faulty Relationship Class in the UserDataModels

---
+++ UserSettings.py
@@ -0,0 +1,44 @@
+
+import os
+import logging
+
+from google.appengine.ext import webapp
+from google.appengine.api import users
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
+
+from MasterHandler import MasterHandler
+
+from UserDataModels import UserProfile, UserSettings, availableLanguages
+
+class Settings(MasterHandler):
+
+  def get(self):
+    user = users.get_current_user()
+    query = UserSettings.all()
+    userSettings = query.filter("user =", user).get()
+
+    template_values = {
+      'preferredLanguage': 'pl',#userSettings.preferredLanguage,
+    }
+    MasterHandler.sendTopTemplate(self, activeEntry = "Settings")
+    MasterHandler.sendContent(self, 'templates/viewSettings.html', template_values)
+    MasterHandler.sendBottomTemplate(self)
+
+class SaveSettings(MasterHandler):
+
+  def get(self):
+    MasterHandler.sendTopTemplate(self, activeEntry = "Settings")
+    self.response.out.write('<div id="content">Under construction</div>')
+    MasterHandler.sendBottomTemplate(self)
+
+application = webapp.WSGIApplication([
+  ('/settings', Settings),
+  ('/savesettings', SaveSettings)
+], debug=True)
+
+def main():
+  run_wsgi_app(application)
+
+if __name__ == '__main__':
+  main()
