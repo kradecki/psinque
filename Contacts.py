c0b03f17cec29ce045518033cea68213ef15f0ef
Krzysztof Radecki <krzysztof.radecki@gmail.com>
2012-07-24 21:34:05 +0200

    Code cosmetics. User search filtering fine tuned...again

--- Contacts.py
+++ Contacts.py
@@ -14,21 +14,23 @@ from UserDataModels import UserProfile, UserSettings, availableLanguages, Relati
 class ViewContacts(MasterHandler):
   
   def get(self):
+    
     user = users.get_current_user()
     query = UserProfile.all()
     userProfile = query.filter("user =", user).get()
-    if user == None:  # no user logged in
+    
+    # If the user is not logged in, redirect to main page
+    if user == None:
       self.redirect("/")
       return
+
+    userContacts = userProfile.outgoingRelationships
       
     template_values = {
-      'message': 'You have currently no contacts',
+      'header': 'Your Contacts:',
+      'userContacts': userContacts,
+      'howManyContacts': userContacts.count(),
     }
-    for relationship in userProfile.outgoingRelationships:
-      logging.info(relationship)
-      template_values = {
-        'message': 'Your contacts',
-      }
 
     MasterHandler.sendTopTemplate(self, activeEntry = "Contacts")
     MasterHandler.sendContent(self, 'templates/viewContacts.html', template_values)
@@ -38,14 +40,18 @@ class SearchContacts(MasterHandler):
 
   def post(self):
 
-    # Implement here search criteria based on supplied information in the form
-    # Ideas: First name, Last name, Email, Telephone
-    # get() or fetch() ? What is the difference?
-    searchProfile = UserProfile.all().filter("firstname =", self.request.get('firstname')).filter("lastname =", self.request.get('lastname'))
+    searchProfile = UserProfile.all()
+
+    # Filter the data based on the supplied search criteria
+    if self.request.get('firstname'):
+      searchProfile.filter("firstname =", self.request.get('firstname'))
+    if self.request.get('lastname'):
+      searchProfile.filter("lastname =", self.request.get('lastname'))
 
     template_values = {
+      'header': 'Search Results:',
       'searchResults': searchProfile,
-      'howManyResults': searchProfile.count()
+      'howManyResults': searchProfile.count(),
     }
 
     MasterHandler.sendTopTemplate(self, activeEntry = "Contacts")
