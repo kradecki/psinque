8fcb62d4452c21de961c4966cfc20fe2d2533e77
Stanisław Raczyński <sutashu@gmail.com>
2012-08-09 22:05:02 +0200

    Cleanup in the page handling; added separate Notifications.py for handling notifications

--- Notifications.py
+++ Notifications.py
@@ -9,14 +9,14 @@ from google.appengine.ext.webapp import template
 
 from MasterHandler import MasterHandler
 
-class Groups(MasterHandler):
+class Notifications(MasterHandler):
   def get(self):
-    MasterHandler.sendTopTemplate(self, activeEntry = "Groups")
+    MasterHandler.sendTopTemplate(self, activeEntry = "Notifications")
     self.response.out.write('<div id="content">Under construction</div>')
     MasterHandler.sendBottomTemplate(self)
 
 application = webapp.WSGIApplication([
-  ('/groups', Groups),
+  ('/notifications', Notifications),
 ], debug=True)
 
 def main():
