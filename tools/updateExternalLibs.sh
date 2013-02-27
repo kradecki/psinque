ad413a49dbf1ab9b28a2328fb22e1b4e23f92953
Stanisław Raczyński <sutashu@gmail.com>
2013-02-27 14:06:05 +0100

    Switched to git version of jQuery & added custom jQuery build & added hg version of WsgiDAV & added tool to update external libs

---
+++ tools/updateExternalLibs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+function checkTools {
+  echo "Checking installed tools..."
+  npm > /dev/null || ( echo "You need to have Node.js installed" && exit 1 )
+  grunt -version > /dev/null || ( echo "You need grunt to compile jQuery" && exit 1)
+}
+
+function updateJQuery {
+  echo "Updating jQuery..."
+  cd jquery
+  git pull    # update the repo
+  npm install # install the jQuery dependencies
+  grunt       # build jQuery
+  cd ..
+}
+
+function updateWsgiDAV {
+  echo "Updating WsgiDAV..."
+  pwd
+  cd wsgidav
+  hg pull -u
+  cd ..
+}
+
+checkTools
+updateJQuery
+updateWsgiDAV
