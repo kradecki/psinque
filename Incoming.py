ffc8643429f413de3102388fb9b4d095202af86c
Stanisław Raczyński <sutashu@gmail.com>
2013-05-14 15:57:46 +0200

    Old code cleanup (removal)

--- Incoming.py
+++ Incoming.py
@@ -5,7 +5,7 @@ import webapp2
 
 from django.utils import simplejson as json
 
-from MasterHandler import MasterHandler
+from MasterHandler import MasterHandler, AjaxError
 from users.UserDataModels import UserProfile, Psinque, UserEmail
 from users.UserManagement import getPublicGroup, getDisplayNameFromPsinque
 from users.Email import notifyPendingPsinque
@@ -16,11 +16,14 @@ class IncomingHandler(MasterHandler):
 
     def get(self, actionName):
         
-        if MasterHandler.safeGuard(self):   # perform action only if user is logged in
+        if MasterHandler.safeGuard(self):
             
             actionFunction = getattr(self, actionName)
-            actionFunction()
-
+            
+            try:
+                actionFunction()
+            except AjaxError as e:
+                self.sendJsonError(e.value)
 
     def view(self):
         
@@ -103,8 +106,9 @@ class IncomingHandler(MasterHandler):
 
     def removepsinque(self):
         
-        psinque = Psinque.get(self.request.get('key'))
-        psinque.delete()
+        psinque = Psinque.get(self.checkGetParameter('key'))
+        if not psinque is None:
+            psinque.delete()
         self.redirect("/incoming")
         
 #-----------------------------------------------------------------------------
--- Outgoing.py
+++ Outgoing.py
@@ -3,43 +3,42 @@ import os
 import logging
 import webapp2
 
-from google.appengine.api import users
-
 from django.utils import simplejson as json
 
-from MasterHandler import MasterHandler
-from users.UserDataModels import UserProfile, Psinque
+from MasterHandler import MasterHandler, AjaxError
+from users.UserDataModels import UserProfile, Psinque, UserEmail
+from users.UserManagement import getPublicGroup, getDisplayNameFromPsinque
+from users.Email import notifyPendingPsinque
 
-class Outgoing(MasterHandler):
-  
-  def get(self):
+#-----------------------------------------------------------------------------
+
+class OutgoingHandler(MasterHandler):
+
+    def get(self, actionName):
+        
+        if MasterHandler.safeGuard(self):
+            
+            actionFunction = getattr(self, actionName)
+            
+            try:
+                actionFunction()
+            except AjaxError as e:
+                self.sendJsonError(e.value)
+
+    def view(self):
     
-    currentUserProfile = UserProfile.all().filter("user =", users.get_current_user()).fetch(1)[0]
-    notifications = Psinque.all().filter("userTo =", currentUserProfile).filter("status =", "pending")
+        if self.getUserProfile():
+            notifications = Psinque.all().filter("userTo =", self.userProfile).filter("status =", "pending")
 
-    template_values = {
-      'notifications': notifications,
-      'notificationCount': notifications.count()
-    }
+            template_values = {
+                'notifications': notifications,
+                'notificationCount': notifications.count()
+            }
 
-    MasterHandler.sendTopTemplate(self, activeEntry = "Outgoing")
-    MasterHandler.sendContent(self, 'templates/notifications_view.html', template_values)
-    MasterHandler.sendBottomTemplate(self)
-
-class ChangeRelationship(webapp2.RequestHandler):
-
-  def get(self):
-
-    currentUserProfile = UserProfile.all().filter("user =", users.get_current_user()).fetch(1)[0]
-    psinque = Psinque.get_by_id(int(self.request.get('id')))
-    if psinque.userTo.user == users.get_current_user():
-      # We still want to filter only the current user to avoid situation when a user changes
-      # relationships of other users by sending a fake GET query
-      psinque.status = self.request.get('to')
-      psinque.put()
-      self.response.out.write(json.dumps({"status": "ok"}))
+            MasterHandler.sendTopTemplate(self, activeEntry = "Outgoing")
+            MasterHandler.sendContent(self, 'templates/notifications_view.html', template_values)
+            MasterHandler.sendBottomTemplate(self)
 
 app = webapp2.WSGIApplication([
-  ('/outgoing', Outgoing),
-  ('/changerelationship', ChangeRelationship),
+    (r'/outgoing/(\w+)', OutgoingHandler),
 ], debug=True)
