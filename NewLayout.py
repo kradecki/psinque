403af082605d8abc5af5bf1e760c31582b872b3e
Stanisław Raczyński <sutashu@gmail.com>
2013-06-08 19:02:09 +0200

    New layout draft created

--- NewLayout.py
+++ NewLayout.py
@@ -7,9 +7,22 @@ import jinja2
 jinja_environment = jinja2.Environment(
     loader = jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
+class MenuEntry:
+    url = ""
+    title = ""
+    entryclass = ""
+    def __init__(self, url, title, entryclass = ""):
+        self.url = url
+        self.title = title
+        self.entryclass = entryclass
+
 class NewLayout(webapp2.RequestHandler):
   def get(self):
-    self.response.out.write(jinja_environment.get_template("templates/newlayout.html").render())
+    self.response.out.write(jinja_environment.get_template("templates/newlayout.html").render(menuentries = [
+            MenuEntry("mycard/view", "My card"),
+            MenuEntry("permits/view", "Permits"),
+            MenuEntry("psinques/view", "Psinques"),
+    ]))
 
 app = webapp2.WSGIApplication([
   ('/newlayout', NewLayout),
