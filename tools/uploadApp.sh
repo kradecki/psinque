93c9d4a3ae7b5e403b398257b249d9c285dfc065
Stanisław Raczyński <sutashu@gmail.com>
2013-07-24 23:18:47 +0200

    Screw IE8; JS minification before uploading to GAE

--- tools/uploadApp.sh
+++ tools/uploadApp.sh
@@ -2,14 +2,19 @@
 mkdir -p build
 echo "Copying files to 'build'..."
 cp *.py build/
-cp *.yaml build/
+cp app.yaml build/
+cp index.yaml build/
 cp -R stylesheets build/
 cp -R templates build/
 cp -R static build/
+cp -R carddav build/
+cp -R dateutil build/
+cp -R wsgidav build/
 echo "Minimizing java scripts..."
 mkdir -p build/javascripts
 for filename in javascripts/*.js; do
-  uglifyjs -mt $filename > build/$filename
+  echo " - $filename"
+  uglifyjs $filename -m -c --screw-ie8 -o build/$filename
 done
 echo "Uploading the app..."
-# python2.7 ../google_appengine/appcfg.py update .
+python2.7 ../google_appengine/appcfg.py update build
