5be851993620512289ec1ece2a6af60238564221
Stanisław Raczyński <sutashu@gmail.com>
2013-07-30 12:05:07 +0200

    Improved landing page

--- javascripts/Landing.js
+++ javascripts/Landing.js
@@ -1,11 +1,33 @@
 
+function showRow(tr) {
+    $(tr)
+        .show()
+        .find('td')
+        .wrapInner('<div style="display: none;" />')
+        .parent()
+        .find('td > div')
+        .slideDown(700, function(){
+            var $set = $(this);
+            $set.replaceWith($set.contents());
+        });
+}
+
 $(function() {
   
-  $( document ).tooltip({
+  $(document).tooltip({
       position: {
           my: "left+20 top+5",
           at: "left bottom",
           collision: "flipfit" 
   }});
+  
+  $("#logomyspace").click(function() {
+    
+      console.log("Click");
+    
+      showRow('tr.hiddenelements');
+    
+      return false;
+  });
   
 });
