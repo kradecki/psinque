56bcaf1a083e1ac19f7887b5ce697a6aaf9b0542
Stanisław Raczyński <sutashu@gmail.com>
2013-07-30 14:49:40 +0200

    Added CSS minimization

--- tools/uploadApp.sh
+++ tools/uploadApp.sh
@@ -4,7 +4,6 @@ echo "Copying files to 'build'..."
 cp *.py build/
 cp app.yaml build/
 cp index.yaml build/
-cp -R stylesheets build/
 cp -R templates build/
 cp -R static build/
 cp -R carddav build/
@@ -14,7 +13,13 @@ echo "Minimizing java scripts..."
 mkdir -p build/javascripts
 for filename in javascripts/*.js; do
   echo " - $filename"
-  uglifyjs $filename -m -c --screw-ie8 -o build/$filename
+  uglifyjs $filename -m -c --screw-ie8 > build/$filename
+done
+echo "Minimizing stylesheets..."
+mkdir -p build/javascripts
+for filename in stylesheets/*.css; do
+  echo " - $filename"
+  yuicompressor $filename -o build/$filename
 done
 echo "Uploading the app..."
 python2.7 ../google_appengine/appcfg.py update build
