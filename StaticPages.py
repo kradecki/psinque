ec59040994185351513b40d9d7bebf5fe156d2c2
Stanisław Raczyński <sutashu@gmail.com>
2013-08-30 15:10:40 +0200

    Stub for Getting Started addes

--- StaticPages.py
+++ StaticPages.py
@@ -59,6 +59,11 @@ class StaticPageHandler(StaticHandler):
         self.sendContent('templates/Support.html')
 
 
+    def gettingstarted(self):
+      
+        self.sendContent('templates/GettingStarted.html')
+
+
 #-----------------------------------------------------------------------------
 
 app = webapp2.WSGIApplication([
