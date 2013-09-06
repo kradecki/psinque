fca9941566b770322b27efd335ab1dd58e3f78ba
Stanisław Raczyński <sutashu@gmail.com>
2013-09-06 17:33:38 +0200

    Moved all UI functions to ui.js

--- javascripts/ui.js
+++ javascripts/ui.js
@@ -151,3 +151,60 @@ function uiShowErrorMessage(messageText) {
     $("#dialogbox").dialog("open");
     
 }
+
+function removeElementWithEffects(element) {
+    element.slideUp('fast', function() {
+        element.remove();
+    });
+}
+
+function hideElementWithEffects(element) {
+    element.slideUp('fast');
+}
+
+function showElementWithEffects(element) {
+    element.slideDown('fast');
+}
+
+function startLogoAnimation() {
+    window.onbeforeunload = function() {
+        return "An AJAX query is in progress. Please wait until it's done.";
+    }
+    $("#staticlogo").hide();
+    $("#animatedlogo").show();
+}
+
+function stopLogoAnimation() {
+    window.onbeforeunload = null;
+    $("#staticlogo").show();
+    $("#animatedlogo").hide();
+}
+
+function markChangedFields(where) {
+    $(where).css("color", "#de5d35");
+    $(where).find("input,select").css("color", "#de5d35");
+}
+
+function unmarkChangedFields(where) {
+    $(where).css("color", "inherit");
+    $(where).find("input,select,label").css("color", "inherit");
+}
+
+function unmarkAllFields() {
+    unmarkChangedFields(document);
+}
+
+function psinqueSetMarkingOnChange(where) {
+    $(where).change(function() {
+        markChangedFields(this);
+    });
+}
+
+$(document).ready(function() {
+    
+    $('select').dropdown();
+    
+    psinqueSetMarkingOnChange("input,select");
+    uiInitializeCheckboxes();
+    
+});
